b79fb2a22863b0456323bb1b64bf8e3f2a36bfb9
Kenni Kollemorten <kenn7575@gmail.com>
2023-07-25 17:45:40 +0200

    resolve bugs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import { isMobileDevice } from '../stores/isMobileDevice';
-	import { onMount } from 'svelte';
+
 	import NavBar from '$lib/sections/NavBar.svelte';
 	import Footer from '$lib/sections/Footer.svelte';
-	onMount(() => {
-		let isMobile = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-			navigator.userAgent
-		);
-		isMobileDevice.set(isMobile);
-	});
 </script>
 
 <nav class="sticky top-0 z-40">
