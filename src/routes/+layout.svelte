a3f33c17b1ae4de6508ada46865b234b3adc0c0c
Kenni Kollemorten <kenn7575@gmail.com>
2023-07-25 12:00:37 +0200

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 	import { onMount } from 'svelte';
 	import NavBar from '$lib/sections/NavBar.svelte';
 	import Footer from '$lib/sections/Footer.svelte';
-
 	onMount(() => {
 		let isMobile = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
 			navigator.userAgent
