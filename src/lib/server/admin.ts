7637e7dfbf0f249ef25054c19f379cff64d833b3
Kenni Kollemorten <kenn7575@gmail.com>
2023-07-29 22:04:37 +0200

    production test

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,16 +1,17 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { FB_PROJECT_ID, FB_PRIVATE_KEY, FB_CLIENT_EMAIL } from '$env/static/private';
 import pkg from 'firebase-admin';
 
+const firebaseConfig = {
+	credential: pkg.credential.cert({
+		projectId: process.env.FB_PROJECT_ID,
+		privateKey: process.env.FB_PRIVATE_KEY,
+		clientEmail: process.env.FB_CLIENT_EMAIL
+	})
+};
+
 try {
-	pkg.initializeApp({
-		credential: pkg.credential.cert({
-			projectId: FB_PROJECT_ID,
-			privateKey: FB_PRIVATE_KEY,
-			clientEmail: FB_CLIENT_EMAIL
-		})
-	});
+	pkg.initializeApp(firebaseConfig);
 } catch (error: any) {
 	if (!/already exists/u.test(error.message)) {
 		console.error('Firebase admin initialization error', error.stack);
