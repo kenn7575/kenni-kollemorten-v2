35ff6cfc0424be793eb4912430117f3af6c8bc87
Kenni Kollemorten <kenn7575@gmail.com>
2023-10-13 00:17:43 +0300

    fixes to hosting config

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,63 +1,28 @@
-// import { getAuth } from 'firebase-admin/auth';
-// import { getFirestore } from 'firebase-admin/firestore';
-// import { getStorage } from 'firebase-admin/storage';
-
-// // import { FB_PROJECT_ID, FB_PRIVATE_KEY, FB_CLIENT_EMAIL } from '$env/static/private';
-// import pkg from 'firebase-admin';
-
-// try {
-// 	//Vercel
-// 	const raw = process.env.FB_PRIVATE_KEY;
-// 	const key = raw?.replaceAll('[REPLACE]', '\n');
-// 	pkg.initializeApp({
-// 		credential: pkg.credential.cert({
-// 			projectId: process.env.FB_PROJECT_ID,
-// 			privateKey: key,
-// 			clientEmail: process.env.FB_CLIENT_EMAIL
-// 		})
-// 	});
-
-// 	//Local
-// 	// pkg.initializeApp({
-// 	// 	credential: pkg.credential.cert({
-// 	// 		projectId: FB_PROJECT_ID,
-// 	// 		privateKey: FB_PRIVATE_KEY,
-// 	// 		clientEmail: FB_CLIENT_EMAIL
-// 	// 	})
-// 	// });
-// } catch (error: any) {
-// 	if (!/already exists/u.test(error.message)) {
-// 		console.error('Firebase admin initialization error', error.stack);
-// 	}
-// }
-
-// export const adminDB = getFirestore();
-// export const adminAuth = getAuth();
-// export const adminStorage = getStorage();
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
+// import { FB_PROJECT_ID, FB_PRIVATE_KEY, FB_CLIENT_EMAIL } from '$env/static/private';
 import pkg from 'firebase-admin';
 
-// import { FB_PROJECT_ID, FB_PRIVATE_KEY, FB_CLIENT_EMAIL } from '$env/static/private';
-
 try {
-	//vercel
+	//Vercel
+	const raw = process.env.FB_PRIVATE_KEY;
+	const key = raw?.replaceAll('[REPLACE]', '\n');
 	pkg.initializeApp({
 		credential: pkg.credential.cert({
-			projectId: process.env.FB_PROJECT_ID!,
-			privateKey: process.env.FB_PRIVATE_KEY!.replace('[REPLACE]', '\n'),
-			clientEmail: process.env.FB_CLIENT_EMAIL!
+			projectId: process.env.FB_PROJECT_ID,
+			privateKey: key,
+			clientEmail: process.env.FB_CLIENT_EMAIL
 		})
 	});
 
-	// Local
+	//Local
 	// pkg.initializeApp({
 	// 	credential: pkg.credential.cert({
-	// 		projectId: FB_PROJECT_ID!,
-	// 		privateKey: FB_PRIVATE_KEY!,
-	// 		clientEmail: FB_CLIENT_EMAIL!
+	// 		projectId: FB_PROJECT_ID,
+	// 		privateKey: FB_PRIVATE_KEY,
+	// 		clientEmail: FB_CLIENT_EMAIL
 	// 	})
 	// });
 } catch (error: any) {
